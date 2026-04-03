6d83563086ea2fad765a029e53eb7a7ff6c5776c
VovaKondrasiuk <117572689+VovaKondrasiuk@users.noreply.github.com>
2026-04-03 19:53:22 +0300

    Create main.tf

---
+++ task1/main.tf
@@ -0,0 +1,84 @@
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_vpc" "main" {
+  name     = "${var.project_prefix}-vpc"
+  region   = var.region
+  ip_range = var.vpc_cidr
+}
+
+resource "digitalocean_droplet" "node" {
+  name     = "${var.project_prefix}-node"
+  region   = var.region
+  size     = var.droplet_size
+  image    = "ubuntu-24-04-x64"
+  vpc_uuid = digitalocean_vpc.main.id
+  ssh_keys = var.ssh_key_ids
+  tags     = ["${var.project_prefix}-node"]
+}
+
+resource "digitalocean_firewall" "main" {
+  name = "${var.project_prefix}-firewall"
+
+  droplet_ids = [digitalocean_droplet.node.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8000"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8001"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8002"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8003"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_spaces_bucket" "main" {
+  name   = "${var.project_prefix}-bucket"
+  region = var.region
+}
