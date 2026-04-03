4724ba63bea46525e7aa6d1a9521269768fe9329
VovaKondrasiuk <117572689+VovaKondrasiuk@users.noreply.github.com>
2026-04-03 19:52:28 +0300

    Add Terraform configuration for DigitalOcean backend

---
+++ task1/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+
+    bucket = "kondrasiuk-tfstate"
+    key    = "task1/terraform.tfstate"
+    region = "us-east-1"
+
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    use_lockfile                = true
+  }
+}
